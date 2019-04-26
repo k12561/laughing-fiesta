12d297a1aef1776cbd1b946e52121a1813c51cfe
Kent Huang <k-huang1@hotmail.com>
2019-04-25 17:47:37 -0700

    Add files via upload

---
+++ .gitignore/myCalculator/app/src/main/java/com/test/mycalculator/MainActivity.kt
@@ -0,0 +1,94 @@
+/*  Description:  Calculator from https://www.youtube.com/watch?v=EpP6KgJtHTk,
+        first time following a tutorial on for kotlin
+
+*   Author: Kent Huang
+*   Date: April 2019
+*
+*
+*/
+
+package com.test.mycalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Numbers
+        tvOne.setOnClickListener{ appendOnExpression(  "1",  true ) }
+        tvTwo.setOnClickListener{ appendOnExpression(  "2",  true ) }
+        tvThree.setOnClickListener{ appendOnExpression(  "3",  true ) }
+        tvFour.setOnClickListener{ appendOnExpression(  "4",  true ) }
+        tvFive.setOnClickListener{ appendOnExpression(  "5",  true ) }
+        tvSix.setOnClickListener{ appendOnExpression(  "6",  true ) }
+        tvSeven.setOnClickListener{ appendOnExpression(  "7",  true ) }
+        tvEight.setOnClickListener{ appendOnExpression(  "8",  true ) }
+        tvNine.setOnClickListener{ appendOnExpression(  "9",  true ) }
+        tvZero.setOnClickListener{ appendOnExpression(  "0",  true ) }
+        tvDot.setOnClickListener{ appendOnExpression(  ".",  true ) }
+
+        // Operators
+        tvPlus.setOnClickListener{ appendOnExpression("+", false) }
+        tvMinus.setOnClickListener{ appendOnExpression("-", false)}
+        tvMul.setOnClickListener{ appendOnExpression("*", false)}
+        tvDivide.setOnClickListener{ appendOnExpression("/", false)}
+        tvOpen.setOnClickListener{ appendOnExpression("(", false)}
+        tvClose.setOnClickListener{ appendOnExpression(")", false)}
+
+        //  What setting the text to equal nothing on clicking clear button
+        tvClear.setOnClickListener{
+            tvExpression.text = ""
+            tvResult.text = ""
+        }
+
+        //  Delete button
+        tvBack.setOnClickListener{
+            val string = tvExpression.text.toString()
+            if(string.isNotEmpty()){
+                tvExpression.text = string.substring(0,string.length-1)
+            }
+            tvResult.text = ""
+        }
+
+        //  What to do when equals sign is pressed, uses try catch learned before in class
+        tvEquals.setOnClickListener{
+            try{
+                val expression = ExpressionBuilder(tvExpression.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+                if(result == longResult.toDouble()){
+                    tvResult.text = longResult.toString()
+                }
+                else
+                    tvResult.text = result.toString()
+            }catch(e:Exception){
+                Log.d("Exception", "message : " + e.message)
+            }
+        }
+    }
+
+    fun appendOnExpression( string: String,  canClear : Boolean){
+
+        if(tvResult.text.isNotEmpty()) {
+            tvExpression.text = ""
+        }
+
+
+        if(canClear) {
+            tvResult.text = ""
+            tvExpression.append(string)
+        }else{
+                tvExpression.append(tvResult.text)
+                tvExpression.append(string)
+                tvResult.text = ""
+            }
+    }
+
+}
